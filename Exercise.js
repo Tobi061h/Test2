0e1e131ae2b2961a5a9f2a104f4e3ae8459646bb
Tobi061h <47140385+Tobi061h@users.noreply.github.com>
2019-01-30 10:45:49 +0100

    Test 2
    Dette er test 2

---
+++ Exercise.js
@@ -0,0 +1,29 @@
+/*function calculate() 
+{
+	var t = widthBox.value;
+	//read from the textbox
+	var w = document.getElementById("widthBox").value;
+	var l = document.getElementById("lenghtBox").value;
+	var h = document.getElementById("heightBox").value;
+	//convert to Float
+	w=parseFloat(w);
+	h=parseFloat(h);
+	l=parseFloat(l);
+	var result = w*l*h;
+	//show result in div
+	document.getElementById("result").innerHTML = result;
+}
+*/
+
+function multiplication()
+{
+	//read from textbox
+	document.getElementById("mult-tb").innerHTML ="";
+	var f = document.getElementById("factor").value;
+	for(var i=1;i<=10;i++)
+	{
+		var res=f*i;
+		//show result in div
+		document.getElementById("mult-tb").innerHTML += res+"<br/>";
+	}
+}
